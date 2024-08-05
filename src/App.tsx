b85a09f9333bc9085542d136ac706b92ee3ed636
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-08-05 18:37:58 +0500

    initial setup with dependencies and the ui design search

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { Loader2 } from "lucide-react";
 function App() {
   return (
     <div className="min-h-screen bg-black min-w-full flex justify-center items-center flex-col">
-      <h1 className="text-5xl font-josefinSans text-white">HOME STOVE</h1>
-      <p className="text-3xl font-montserrat text-white mt-5">UI/UX in process</p>
-      <Button disabled className=" mt-5">
-        <Loader2 className="animate-spin">
-        Processing
-          </Loader2> 
-        </Button>
+      <h1 className="text-4xl font-josefinSans text-white">HOME STOVE</h1>
+      <p className="text-2xl font-montserrat text-white mt-5">
+        UI/UX in process
+      </p>
+      <Button disabled variant={"default"} className=" mt-5">
+        <Loader2 className="animate-spin">Processing</Loader2>
+      </Button>
     </div>
   );
 }
